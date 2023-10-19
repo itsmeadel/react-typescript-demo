83cf51c78e8461dbfd97e3102c26ce10549bbaba
adeldiola <adelainediola@gmail.com>
2023-10-19 14:24:47 +0300

    props as array

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import './App.css';
 import {Greet} from "./components/Greet";
 import { Person } from "./components/Person";
+import { PersonList } from './components/PersonList';
 
 function App() {
 
@@ -9,10 +10,28 @@ function App() {
     first: 'Adele',
     last: 'Diola',
   }
+
+ 
+  const nameList = [
+    {
+    first: 'Carl',
+    last: 'J.'
+    },
+    {
+      first: 'Mary',
+      last: 'A.'
+      },
+      {
+        first: 'Elij',
+        last: 'N.'
+        },
+  ]
   return (
     <div className="App">
       <Greet name="Adele" messageCount={20} isLoggedIn={true}/>
       <Person name={personName}/>
+      <PersonList names={nameList}/>
+  
     </div>
   );
 }
