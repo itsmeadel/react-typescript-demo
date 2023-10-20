00a9b6508b1ef8a7af4165e5861bb6ed35bb1bc5
adeldiola <adelainediola@gmail.com>
2023-10-20 19:51:07 +0300

    container and input props

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import {Status } from './components/Status';
 import { Heading } from './components/Heading';
 import { Oscar } from './components/Oscar';
 import { Buttons } from './components/Button';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
 
@@ -41,6 +43,8 @@ function App() {
       <Buttons handleClick ={ (event, id) => {
         console.log('Button Clicked', event)
       }}/>
+      <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles= {{ border: '1px solid black', padding: '1rem'}} />
   
     </div>
   );
