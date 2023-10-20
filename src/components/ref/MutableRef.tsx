ad41f01091a37ad345b14ebdc805e90a21c93168
adeldiola <adelainediola@gmail.com>
2023-10-21 00:55:52 +0300

    useRef hook

---
+++ src/components/ref/MutableRef.tsx
@@ -0,0 +1,33 @@
+import {useState, useRef, useEffect } from 'react'
+
+
+export const MutableRef =() => {
+    const [timer, setTimer] = useState(0)
+    const interValRef = useRef<number | null>(null)
+
+    const stopTimer = () => {
+        if (interValRef.current)
+        window.clearInterval(interValRef.current)
+    }
+
+
+    //invoke the timer value by 1 per second
+    useEffect(() => {
+        interValRef.current = window.setInterval(() => {
+            setTimer((timer) => timer + 1) 
+        }, 1000)
+        return () => {
+            stopTimer()
+        }
+        }, [])
+    
+
+
+
+    return(
+        <div>
+            HookTimer - {timer} -
+            <button onClick={() => stopTimer()}> Stop Timer </button> 
+        </div>
+    )
+}
