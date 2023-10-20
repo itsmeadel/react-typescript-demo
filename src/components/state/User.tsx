80fcb0fd60896f54bcd68587a4dc332caeea95ec
adeldiola <adelainediola@gmail.com>
2023-10-20 21:35:19 +0300

    useState Future Value

---
+++ src/components/state/User.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+
+
+export const User = () => {
+
+    const [user, setUser] = useState<AuthUser | null>(null) 
+    //user are not loggedin in website. So 'null' as a default function os state
+    //<AuthUser | null> informing typeScript 
+
+    const handleLogin = () =>{
+        setUser({
+            name: 'Adie',
+            email:'adie@example.com',
+        })
+    }
+
+    const handleLogout = () => {
+        setUser(null)
+    }
+    return(
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is {user?.name} </div> 
+            <div>User email is {user?.email} </div>
+        </div> //user? = asking if the object exists
+    )
+}
