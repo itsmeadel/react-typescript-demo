8dbb132744285e15db8e6682c731fbcdf3a6b3c4
adeldiola <adelainediola@gmail.com>
2023-10-20 21:45:31 +0300

    useState type Assertion

--- src/components/state/User.tsx
+++ src/components/state/User.tsx
@@ -12,6 +12,7 @@ export const User = () => {
     const [user, setUser] = useState<AuthUser | null>(null) 
     //user are not loggedin in website. So 'null' as a default function os state
     //<AuthUser | null> informing typeScript 
+   //{} as AuthUseras = type assertion | to let typeScript know that user is a typeOf user and won't be null
 
     const handleLogin = () =>{
         setUser({
@@ -29,6 +30,8 @@ export const User = () => {
             <button onClick={handleLogout}>Logout</button>
             <div>User name is {user?.name} </div> 
             <div>User email is {user?.email} </div>
-        </div> //user? = asking if the object exists
+        </div>
+         //user? = asking if the object exists that can be null
+         
     )
 }
